22e475fdef66725e311bfa1dbe561d8d5bb4fe19
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-16 13:05:12 +0100

    ....

---
+++ main.js
@@ -0,0 +1,44 @@
+let book = prompt ("Enter Name of book:");
+let author = prompt ("Enter Authors' Name:");
+let qty = prompt ("Enter quantity needed:");
+
+const data = {
+
+}
+
+data[book] = author;
+console.log(data);
+
+
+NB.innerHTML = book;
+NM.innerHTML = author;
+QT.innerHTML = qty;
+
+
+
+let Book = prompt ("Enter Name of book:");
+let Author = prompt ("Enter Authors' Name:");
+let Qty = prompt ("Enter quantity needed:");
+data[Book] = Author;
+
+
+
+NB1.innerHTML = Book;
+NM1.innerHTML = Author;
+QT1.innerHTML = Qty;
+
+
+
+let booK = prompt ("Enter Name of book:");
+let authoR = prompt ("Enter Authors' Name:");
+let qtY = prompt ("Enter quantity needed:");
+data[booK] = authoR;
+
+
+
+NB2.innerHTML = booK;
+NM2.innerHTML = authoR;
+QT2.innerHTML = qtY;
+
+
+
